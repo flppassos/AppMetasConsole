ce8f8e75f5c13f98b451845c73d8937d8cb26d90
Felipe Passos Ferreira <felipepassos08@gmail.com>
2024-11-03 18:56:42 -0300

    commit initial

---
+++ index.js
@@ -0,0 +1,17 @@
+const start = () => {
+
+    while(true) {
+        
+        let op = "1 - Cadastrar";
+
+        switch (op) {
+            case "1 - Cadastrar":
+                console.log('Cadastrar');
+                break
+        }
+
+        return
+    }
+} 
+
+start();
